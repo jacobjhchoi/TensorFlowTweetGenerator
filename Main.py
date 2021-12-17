d73c6cf523a265f32c89ba601d98f8c1262df534
Jacob <77601745+jacobjhchoi@users.noreply.github.com>
2021-12-16 19:25:33 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
@@ -17,22 +16,15 @@ wordCount = len(tokenizer.word_index) + 1
 
 input_sequences = []
 for line in training:
-	token_list = tokenizer.texts_to_sequences([line])[0]
-	for i in range(1, len(token_list)):
-		n_gram_sequence = token_list[:i+1]
-		input_sequences.append(n_gram_sequence)
+    tokenized_texts = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(tokenized_texts)):
+        input_sequences.append(tokenized_texts[:i + 1])
 
 max_sequence_length = max([len(x) for x in input_sequences])
 input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre'))
 
-# create predictors and label
-xs = input_sequences[:,:-1]
-labels = input_sequences[:,-1]
-ys = tf.keras.utils.to_categorical(labels, num_classes=wordCount)
-
 # Load model
 loaded_model = tf.keras.models.load_model("TrumpModel.h5")
-
 
 # Make a prediction
 flag = True
@@ -40,12 +32,13 @@ next_words = 20
 while (flag):
 
     input_text = input("Enter the starting text: ")
-    if input_text == 'quit':
+    if (input_text == '-1'):
         flag = False
+
     for i in range(next_words):
-        token_list = tokenizer.texts_to_sequences([input_text])[0]
-        token_list = pad_sequences([token_list], maxlen=max_sequence_length - 1, padding='pre')
-        predicted = np.argmax(loaded_model.predict(token_list), axis=-1)
+        tokenized_texts = tokenizer.texts_to_sequences([input_text])[0]
+        tokenized_texts = pad_sequences([tokenized_texts], maxlen=max_sequence_length - 1, padding='pre')
+        predicted = np.argmax(loaded_model.predict(tokenized_texts), axis=-1)
         output_word = ""
         for word, index in tokenizer.word_index.items():
             if index == predicted:
